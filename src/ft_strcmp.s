6e4bd3720b11866be7322320e5ae02029fa04c7a
octoross-42 <codingoceane@gmail.com>
2026-06-24 02:43:24 +0200

    fix(strcmp): wrong order for substraction

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -5,15 +5,15 @@ ft_strcmp:
 	xor rax, rax
 	xor r10d, r10d
 	.loop:
-		mov r10b, [rdi + rax]
-		cmp r10b, [rsi + rax]
+		mov r10b, [rsi + rax]
+		cmp r10b, [rdi + rax]
 		jne .return
 		test r10b, r10b
 		jz .return
 		inc rax
 		jmp .loop
 	.return:
-		movzx eax, byte [rsi + rax]
+		movzx eax, byte [rdi + rax]
 		sub eax, r10d
 		ret
 
